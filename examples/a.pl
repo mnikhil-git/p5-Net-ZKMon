5acfa544ed61acf19023dc4737155eff01f36c94
Nikhil Mulley <nikhil.mulley@inmobi.com>
2012-09-30 16:04:41 +0530

    modified example a.pl script

--- examples/a.pl
+++ examples/a.pl
@@ -3,14 +3,19 @@
 use Net::ZKMon;
 use Data::Dumper;
 
-my $zkmon = new Net::ZKMon(hostname => 'zookeeper1.corp.mycompany.com');
+my $zkhost1 = 'zookeeper1.corp.mycompany.com';
+my $zkhost2 = 'zookeeper2.corp.mycompany.com';
+
+my $zkmon = new Net::ZKMon(hostname => $zkhost1);
 
 print Dumper($zkmon->stat);
+
+print "Version being used is : " , $zkmon->stat->{'version'}, "\n";
 
 $zkmon = new Net::ZKMon;
 
 print Dumper($zkmon->envi);
 
-print Dumper($zkmon->envi('zookeeper2.corp.mycompany.com'));
+print Dumper($zkmon->envi($zkhost2));
 
 
